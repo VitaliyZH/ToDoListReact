a93a539cfbb8a1bdf0feb1fe8b83799d4f178091
VitaliyZH <zhilyakov.vitaliy@yandex.ru>
2022-02-11 15:23:21 +0400

    few components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        a > ToDo List < /a> <
-        TaskWrapper / >
-        <
-        /header> <
-        /div>
-    );
+  const [data, setData] = useState([]);
+  const [inputText, setInputText] = useState("");
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>ToDo List</a>
+        <TaskWrapper data={data} />
+        <input type="text" onChange={(event) => setInputText(event.target.value)}></input>
+        <button onClick={() => setData(AddTask(data, inputText))}>Add</button>
+      </header>
+    </div>
+  );
 }
 
-function TaskWrapper() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Wrapper < /h1> <
-        /div>
-    );
+function AddTask(arr, text) {
+  let count;
+  if (arr.length === 0) {
+    count = 0;
+  } else {
+    count = arr[arr.length - 1].id + 1;
+  }
+  let newTask = {
+    id: count,
+    value: text,
+    type: "active",
+  };
+  arr.push(newTask);
+  return arr;
 }
 
+function TaskWrapper(props) {
+  const activeTasks = props.data.filter((el) => el.type === "active");
+  const completedTasks = props.data.filter((el) => el.type === "completed");
+  return (
+    <div className="TaskWrapper">
+      <TaskColumn name="Active" taskList={activeTasks} />
+      <TaskColumn name="Completed" taskList={completedTasks} />
+    </div>
+  );
+}
+
+function TaskColumn(props) {
+  const taskList = props.taskList;
+
+  return (
+    <div className="TaskColumn">
+      <h1>{props.name}</h1>
+      {taskList.map((el) => (
+        <Task text={el.text} />
+      ))}
+    </div>
+  );
+}
+
+function Task(props) {
+  return (
+    <div className="Task">
+      <input type="checkbox"></input>
+      <p>{props.text}</p>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  );
+}
 
 export default App;
 export { TaskWrapper };
